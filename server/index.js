e7553652cf866b06643a78cdb92c4568a87e78ef
Connor15790 <ConnorKenway157@gmail.com>
2024-08-11 00:42:26 +0530

    changed stuff

--- server/index.js
+++ server/index.js
@@ -22,6 +22,9 @@ mongoose.connect(DB_URL, {
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
